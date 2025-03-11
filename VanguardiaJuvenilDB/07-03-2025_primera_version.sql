051378a0537a83c966264f0af613629db01be46a
Nadissa <vel23764@uvg.edu.gt>
2025-03-10 18:39:56 -0600

    Foreign key tipe fixed

--- VanguardiaJuvenilDB/07-03-2025_primera_version.sql
+++ VanguardiaJuvenilDB/07-03-2025_primera_version.sql
@@ -1,22 +1,22 @@
 -- Creación Tabla Usuarios
 create table Usuarios (
-    id int primary key not null,
+    id serial primary key not null,
     rol varchar(50) not null
 );
 
 -- Creación Tabla Materias
 create table Materiales (
-    id int primary key not null,
+    id serial primary key not null,
     nombre varchar(50) not null
 );
 
 -- Creación Tabla Grados
 create table Grados (
-    id int primary key not null,
+    id serial primary key not null,
     grado varchar(50) not null
 );-- Creación Tabla Maestros
 create table Maestros (
-  id int primary key not null,
+  id serial primary key not null,
   rol int not null,
   nombre varchar(50) not null,
   apellido varchar(50) not null,
@@ -38,7 +38,7 @@ create table Estudiantes (
 )
 -- Creación tabla Padres
 Create table Padres (
-  id int primary key not null,
+  id serial primary key not null,
   carnet_estudiante int not null, --ver si lo hacemos unique por ser clave de acceso al portal
   rol int not null,
   apellido_estudiante varchar(50) not null,
@@ -51,7 +51,7 @@ Create table Padres (
 -- Creación tabla Familia
 -- Tabla para relacionar a los padres/tutores con los alumnos
 Create table Familias (
-  id int primary key not null,
+  id serial primary key not null,
   id_padre int not null,
   id_estudiante int not null,
   foreign key (id_padre) references padres (id),
@@ -59,7 +59,7 @@ Create table Familias (
 )
 -- Creación tabla Administrativos
 Create table Administrativos (
-  id int primary key not null,
+  id serial primary key not null,
   rol int not null,
   nombre varchar(50) not null,
   apellido varchar(50) not null,
@@ -70,20 +70,20 @@ Create table Administrativos (
 )
 -- Creación tabla Secciones
 Create table Secciones (
-  id int primary key not null,
+  id serial primary key not null,
   id_estudiante int not null,
   foreign key (id_estudiante) references estudiantes (carnet)
 )
 -- Creación tabla pagos
 Create table Pagos (
 	id serial primary key not null,
-	id_padre serial references Padres(id)
-	id_estudiante serial references Estudiantes(id)
+	id_padre int references Padres(id)
+	id_estudiante int references Estudiantes(id)
 )
 -- Creación tabla solvencias
 Create table Solvencias (
 	id serial primary key not null,
-	id_pagos serial references Pagos (id) not null,
+	id_pagos int references Pagos (id) not null,
 	mes_solvencia date not null,
 	fecha_pago date not null
 )
@@ -91,15 +91,15 @@ Create table Solvencias (
 -- Creación tabla cursos
 Create table Cursos (
 	id serial primary key not null,
-	id_materia serial not null references Materias(id),
-	id_maestro serial not null references Maestros(id),
-	id_seccion serial not null references Secciones(id),
+	id_materia int not null references Materias(id),
+	id_maestro int not null references Maestros(id),
+	id_seccion int not null references Secciones(id),
 	id_grado serial not null references Grados(id)
 )
 
 -- Creación tabla 
 Create table Cursos_tareas (
 	id serial primary key not null,
-	id_curso serial not null references Cursos(id),
-	id_tareas serial not null references Tareas(id)
+	id_curso int not null references Cursos(id),
+	id_tareas int not null references Tareas(id)
 )
